d581cf4d71d4fe849e7005d7e7bdf29c33ce7f82
Ben Slaughter <bens@brandwatch.com>
2014-07-07 16:24:06 +0100

    Added Utilise::HTTP::FirstLine#split and tests

--- spec/http/first_line_spec.rb
+++ spec/http/first_line_spec.rb
@@ -6,16 +6,34 @@ describe Utilise::HTTP::FirstLine do
       expect(Utilise::HTTP::FirstLine.type("string")).to be_a_kind_of(Symbol)
     end
 
-    it "returns request on a request line" do
+    it "returns request on a request firstline" do
       expect(Utilise::HTTP::FirstLine.type("GET * HTTP/1.1")).to be :request
     end
 
-    it "returns response on a response line" do
+    it "returns response on a response firstline" do
       expect(Utilise::HTTP::FirstLine.type("HTTP/1.1 200 OK")).to be :response
     end
 
-    it "returns unknown on an unknown line" do
+    it "returns unknown on an unknown firstline" do
       expect(Utilise::HTTP::FirstLine.type("unknown")).to be :unknown
+    end
+  end
+
+  describe "#split" do
+    it "returns a array" do
+      expect(Utilise::HTTP::FirstLine.split("GET * HTTP/1.1")).to be_a_kind_of(Array)
+    end
+
+    it "returns an array of request elements" do
+      expect(Utilise::HTTP::FirstLine.split("GET * HTTP/1.1")).to match_array ["GET", "*", "1", "1"]
+    end
+
+    it "returns an array of request elements" do
+      expect(Utilise::HTTP::FirstLine.split("HTTP/1.1 200 OK")).to match_array ["1", "1", "200", "OK"]
+    end
+
+    it "returns nil if firstline unknown" do
+      expect(Utilise::HTTP::FirstLine.split("unknown")).to be_nil
     end
   end
 end
