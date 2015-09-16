fd61b69c2b04b9d3df114c19f612505e4e8ae7a5
Ben Slaughter <bens@brandwatch.com>
2015-09-16 11:59:13 +0100

    Patch version 0.6.1

--- lib/utilise/version.rb
+++ lib/utilise/version.rb
@@ -1,9 +1,9 @@
 # Utilise
 module Utilise
   # The current gem version
-  VERSION = '0.6.0'
+  VERSION = '0.6.1'
   # The version update date
-  DATE = '2015-09-10'
+  DATE = '2015-09-16'
   # Debug output message
   MSG = 'Version %s %s (running on %s-%s)'
 
