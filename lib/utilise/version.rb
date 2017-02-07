fd80def027b89194028b3d0258d7c72079ee43ae
Ben Slaughter <bens@brandwatch.com>
2017-02-07 11:16:30 +0000

    Version 1.1.0

--- lib/utilise/version.rb
+++ lib/utilise/version.rb
@@ -1,9 +1,9 @@
 # Utilise
 module Utilise
   # The current gem version
-  VERSION = '1.0.0'.freeze
+  VERSION = '1.1.0'.freeze
   # The version update date
-  DATE = '2015-10-06'.freeze
+  DATE = '2017-02-07'.freeze
   # Debug output message
   MSG = 'Version %s %s (running on %s-%s)'.freeze
 
